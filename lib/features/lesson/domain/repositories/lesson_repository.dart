4af6f95222fdd27378605d559dee2b02259ebbca
Eduardo Ferreira <eduardosilvaferreira4@gmail.com>
2026-05-31 22:36:46 -0300

    DEL: trilha global e catálogo de matérias (legado do MVP)

--- lib/features/lesson/domain/repositories/lesson_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dartz/dartz.dart';
-
-import '../../../../core/errors/failure.dart';
-import '../entities/lesson.dart';
-
-abstract class LessonRepository {
-  Future<Either<Failure, List<Lesson>>> getAllLessons();
-  Future<Either<Failure, Lesson>> getLessonById(String id);
-}
